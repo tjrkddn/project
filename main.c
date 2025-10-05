a59b5f7c9610cd5d3efe0f6695152922e158f400
tjrkddn <moonuma777@gmail.com>
2025-10-05 18:24:41 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+     char message2[5] = {'a','b','c','d', '\0'};
+    char message[5] = {'a','b','c','\0'};
+   
+    
+    printf("message = %s, message2 = %s",message,message2);
+    return 0;
+}
